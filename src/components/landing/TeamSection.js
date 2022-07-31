8dccbd6793234765e3890afff58be1c5c215ef67
Khoirul01 <gamma.dev@nexagroup.co.id>
2022-07-31 22:53:27 +0700

    fitur pencarian menu

--- src/components/landing/TeamSection.js
+++ src/components/landing/TeamSection.js
@@ -1,23 +1,40 @@
 /* eslint-disable array-callback-return */
 import Title from './../../components/landing/Title';
 import RestaurantCard from './../../components/landing/RestaurantCard';
+import Input from '@material-tailwind/react/Input';
+import Button from '@material-tailwind/react/Button';
 import Api from './../../Api';
 import React, {useEffect, useState} from 'react';
 
 export default function TeamSection() {
     const [meal, setMeal] = useState([]);
+    const [keyword, setKeyword] = useState('');
+    const [msg, setMsg] = useState('...Loading...');
     useEffect(()=>{
-        dataMenuRestaurant();
+        dataMenuRestaurant('');
     },[]);
-    const dataMenuRestaurant = () => {
-        Api.get('search.php?s=')
+    const dataMenuRestaurant = (key) => {
+        Api.get(`search.php?s=${key}`)
         .then(body => {
-            setMeal(body.data.meals)
+            if(body.data.meals !== null){
+                setMeal(body.data.meals);
+            } else {
+                setMsg("Pencarian Tidak Ada");
+            }
         })
         .catch(err => {
             console.log(err, 'error');
         });
     };
+    const setQuery = (event) => {
+        setKeyword(event.target.value)
+    }
+    const search = (event) => {
+        event.preventDefault();
+        setMsg('...Loading...')
+        setMeal([]);
+        dataMenuRestaurant(keyword);
+    }
     return (
         <section className="pt-20 pb-48">
             <div className="container max-w-7xl mx-auto px-4">
@@ -25,6 +42,19 @@ export default function TeamSection() {
                     Ayo Jelajahi Menu Restaurant yang mungkin bisa
                     anda jadikan destinasi tempat anda makan dan liburan.
                 </Title>
+                <form onSubmit={search}>
+                    <div className="flex gap-4 mt-16 mb-12 mr-10">
+                                <Input
+                                    type="text"
+                                    placeholder="Ketik Menu"
+                                    onChange={setQuery}
+                                    color="lightBlue"
+                                />
+                                <Button color="lightBlue" ripple="light" type="submit">
+                                    Cari
+                                </Button>
+                    </div>
+                </form>
                 <div className="flex flex-wrap">
                     {meal.length !== 0 ?
                     meal.map((data)=>{
@@ -34,7 +64,7 @@ export default function TeamSection() {
                         name={data.strMeal}
                         visi={`${data.strInstructions.substring(0, 40)}....`}
                         />)
-                    }) : <p>....Loading...</p>}
+                    }) : <p>{msg}</p>}
                 </div>
             </div>
         </section>
